fcba91ced94daf2ca94d11bc7a47bed7ae8611e0
nami <cigarbox.nami@gmail.com>
2020-06-02 17:19:18 +0900

    supported movie

---
+++ scripts/demo_movie.py
@@ -0,0 +1,35 @@
+from scripts.removal_background import *
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # カメラ画像の横幅を1280に設定
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # カメラ画像の縦幅を720に設定
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('H', '2', '6', '4'))
+
+    i = 0
+
+    ret, frame = cap.read()
+
+    bg = np.full_like(frame, 255., dtype=float)
+    # bg = bg.astype(float)
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            continue
+
+        removed = removal_background(frame, bg)
+
+        # cv2.imwrite("./examples/outs/{}.png".format(i), removed)
+
+        removed = removed.astype(np.uint8)
+
+        cv2.imshow("removal_background", removed)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
--- scripts/removal_background.py
+++ scripts/removal_background.py
@@ -15,10 +15,10 @@ from hlmobilenetv2 import hlmobilenetv2
 
 # ignore warnings
 import warnings
+
 warnings.filterwarnings("ignore")
 
-
-IMG_SCALE = 1./255
+IMG_SCALE = 1. / 255
 IMG_MEAN = np.array([0.485, 0.456, 0.406, 0]).reshape((1, 1, 4))
 IMG_STD = np.array([0.229, 0.224, 0.225, 1]).reshape((1, 1, 4))
 
@@ -28,23 +28,34 @@ RESULT_DIR = './examples/mattes'
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=True)
+model = model.to(device)
+model.eval()
+
+preprocess = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
 if not os.path.exists(RESULT_DIR):
     os.makedirs(RESULT_DIR)
 
 # load pretrained model
 net = hlmobilenetv2(
-        pretrained=False,
-        freeze_bn=True, 
-        output_stride=STRIDE,
-        apply_aspp=True,
-        conv_operator='std_conv',
-        decoder='indexnet',
-        decoder_kernel_size=5,
-        indexnet='depthwise',
-        index_mode='m2o',
-        use_nonlinear=True,
-        use_context=True
-    )
+    pretrained=False,
+    freeze_bn=True,
+    output_stride=STRIDE,
+    apply_aspp=True,
+    conv_operator='std_conv',
+    decoder='indexnet',
+    decoder_kernel_size=5,
+    indexnet='depthwise',
+    index_mode='m2o',
+    use_nonlinear=True,
+    use_context=True
+)
 
 try:
     checkpoint = torch.load(RESTORE_FROM, map_location=device)
@@ -63,9 +74,11 @@ if torch.cuda.is_available():
 # switch to eval mode
 net.eval()
 
+
 def read_image(x):
     img_arr = np.array(Image.open(x))
     return img_arr
+
 
 def image_alignment(x, output_stride, odd=False):
     imsize = np.asarray(x.shape[:2], dtype=np.float)
@@ -77,20 +90,21 @@ def image_alignment(x, output_stride, odd=False):
 
     x1 = x[:, :, 0:3]
     x2 = x[:, :, 3]
-    new_x1 = cv2.resize(x1, dsize=(w,h), interpolation=cv2.INTER_CUBIC)
-    new_x2 = cv2.resize(x2, dsize=(w,h), interpolation=cv2.INTER_NEAREST)
+    new_x1 = cv2.resize(x1, dsize=(w, h), interpolation=cv2.INTER_CUBIC)
+    new_x2 = cv2.resize(x2, dsize=(w, h), interpolation=cv2.INTER_NEAREST)
 
     new_x2 = np.expand_dims(new_x2, axis=2)
     new_x = np.concatenate((new_x1, new_x2), axis=2)
 
     return new_x
 
+
 def inference(image, trimap):
     with torch.no_grad():
         # image, trimap = read_image(image_path), read_image(trimap_path)
         trimap = np.expand_dims(trimap, axis=2)
         image = np.concatenate((image, trimap), axis=2)
-        
+
         h, w = image.shape[:2]
 
         image = image.astype('float32')
@@ -100,48 +114,42 @@ def inference(image, trimap):
         image = image_alignment(image, STRIDE)
         inputs = torch.from_numpy(np.expand_dims(image.transpose(2, 0, 1), axis=0))
         inputs = inputs.to(device)
-        
+
         # inference
         start = time()
         outputs = net(inputs)
         end = time()
 
         outputs = outputs.squeeze().cpu().numpy()
-        alpha = cv2.resize(outputs, dsize=(w,h), interpolation=cv2.INTER_CUBIC)
+        alpha = cv2.resize(outputs, dsize=(w, h), interpolation=cv2.INTER_CUBIC)
         alpha = np.clip(alpha, 0, 1) * 255.
         trimap = trimap.squeeze()
         mask = np.equal(trimap, 128).astype(np.float32)
         alpha = (1 - mask) * trimap + mask * alpha
 
-        running_frame_rate = 1 * float(1 / (end - start)) # batch_size = 1
-        print('framerate: {0:.2f}Hz'.format(running_frame_rate))
+        running_frame_rate = 1 * float(1 / (end - start))  # batch_size = 1
+        # print('framerate: {0:.2f}Hz'.format(running_frame_rate))
         return alpha.astype(np.uint8)
 
 
-def gen_trimap(mask,k_size=(5,5),ite=1):
-    kernel = np.ones(k_size,np.uint8)  # 要素が一の配列を生成する->1画素で処理する周りの画素のサイズ
-    eroded = cv2.erode(mask,kernel,iterations = ite) # 収縮
-    dilated = cv2.dilate(mask,kernel,iterations = ite) # 膨張
-    trimap = np.full(mask.shape,128)
+def gen_trimap(mask, k_size=(5, 5), ite=1):
+    kernel = np.ones(k_size, np.uint8)  # 要素が一の配列を生成する->1画素で処理する周りの画素のサイズ
+    eroded = cv2.erode(mask, kernel, iterations=ite)  # 収縮
+    dilated = cv2.dilate(mask, kernel, iterations=ite)  # 膨張
+    trimap = np.full(mask.shape, 128)
     trimap[eroded > 200] = 255
     trimap[dilated == 0] = 0
     return trimap
 
-def removal_background(img):
-    img = img[...,::-1] #BGR->RGB
-    h,w,_ = img.shape
-    img = cv2.resize(img,(320,320))
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def removal_background(input_img, bg):
+    t1 = time() * 1000.
+    original = np.copy(input_img)
+    img = input_img[..., ::-1]  # BGR->RGB
+    h, w, _ = img.shape
+    img = cv2.resize(img, (320, 320))
 
-    model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=True)
-    model = model.to(device)
-    model.eval();
-
-    preprocess = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+    t2 = time() * 1000.
 
     input_tensor = preprocess(img)
     input_batch = input_tensor.unsqueeze(0).to(device)
@@ -150,39 +158,42 @@ def removal_background(img):
         output = model(input_batch)['out'][0]
     output = output.argmax(0)
     mask = output.byte().cpu().numpy()
-    mask = cv2.resize(mask,(w,h))
-    ret,mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
-    img = cv2.resize(img,(w,h))
+    mask = cv2.resize(mask, (w, h))
+    ret, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
+    t3 = time() * 1000.
+    trimap = gen_trimap(mask, k_size=(10, 10), ite=5)
 
-    trimap = gen_trimap(mask,k_size=(10,10),ite=5)
     # cv2.imwrite('./examples/trimaps/63.png',trimap) # trimapの書き出し
 
     # indexnet_mattingの呼び出し
-    matte = inference(img, trimap)
+    matte = inference(original, trimap)
 
-    img = img[...,::-1]
-    bg = np.full_like(img,255) #white background
-    img = img.astype(float)
-    bg = bg.astype(float)
+    # white background
+    original = original.astype(float)
 
-    matte = cv2.cvtColor(matte, cv2.COLOR_GRAY2BGR)
+    matte = cv2.cvtColor(matte, cv2.COLOR_GRAY2BGR)  # 10~12ms
 
-    matte = matte.astype(float)/255.0
+    matte = matte.astype(float) / 255.0
+    t4 = time() * 1000.
+    original = cv2.multiply(original, matte)  # 18~20ms
+    bg_removed = cv2.multiply(bg, 1.0 - matte)  # 10~20ms
+    t5 = time() * 1000.
 
-    img = cv2.multiply(img, matte)
-    bg = cv2.multiply(bg, 1.0 - matte)
-    return cv2.add(img, bg)
-
+    print("{}, {}, {}, {}".format(t2 - t1, t3 - t2, t4 - t4, t5 - t4))
+    return cv2.add(original, bg_removed)
 
 
 if __name__ == "__main__":
     # imageの読み込みなど
     image_path = "./examples/images/062.png"
     img = cv2.imread(image_path)
-    outImage = removal_background(img)
+
+    bg = np.full_like(img, 255)
+    bg = bg.astype(float)
+
+    outImage = removal_background(img, bg)
     # plt.subplot(1,2,1)
     # plt.imshow(img)
     # plt.subplot(1,2,2)
     # plt.imshow(outImage/255)
     cv2.imwrite('./examples/outs/62.png', outImage)
-    
